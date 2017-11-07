5a4f6fe84e72ddb031548c94608cb586240ded09
zhuchuqnqi <439294170@qq.com>
2017-11-07 16:56:56 +0800

    !!

--- redisSpringBootDemo/src/main/java/com/example/controller/PayController.java
+++ redisSpringBootDemo/src/main/java/com/example/controller/PayController.java
@@ -20,7 +20,7 @@ public class PayController {
 
 		if (payService.checkLimited(payInfo)) {
 
-			resultMsg = "可正常支付!";
+			resultMsg = "可正常支付!!";
 
 		} else {
 			resultMsg = "当日交易金额已超出限额，交易失败！！！";
